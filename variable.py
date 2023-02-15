c21e5f498f38d25bed086fb83ffe5306e79b95db
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2023-02-15 23:52:39 +0600

    variable of python

---
+++ variable.py
@@ -0,0 +1,13 @@
+first_name="Abdullah Al"
+last_name="Mamun"
+
+print("My name is: ", first_name, last_name)
+
+a=100
+b=2
+c=a/b
+print(c)
+
+my_list=["book", "pen", 201, {1,2,3}]
+
+print(my_list)
