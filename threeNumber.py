3c36673411f4be10abf90b0dd2e3049e78930c83
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2023-02-17 23:54:04 +0600

    threeNumber solved with python

---
+++ threeNumber.py
@@ -0,0 +1,12 @@
+
+a=int(input())
+b=int(input())
+c=int(input())
+
+if a>=b and a>=c:
+    print(a, "is largest")
+elif b>=a and b>=c:
+    print(b, "is the leargest")
+else:
+    print(c, "is the leargest")   
+    
